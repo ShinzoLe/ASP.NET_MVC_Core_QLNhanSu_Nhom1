1382ff79f0f9cd47a220cad375b5a4b7b4df0953
ShinzoLe <134534569+ShinzoLe@users.noreply.github.com>
2024-01-26 23:16:25 +0700

    1

---
+++ Nhom1_Ql_NhanSu/Models/Entities/KhoaDaoTao.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+#nullable disable
+
+namespace Nhom1_Ql_NhanSu.Models.Entities
+{
+    public partial class KhoaDaoTao
+    {
+        [Display(Name = "Mã Khoa")]
+        public int MaKhoa { get; set; }
+        [Display(Name = "Tên Khoa")]
+        public string TenKhoa { get; set; }
+        [Display(Name = "Mô Tả")]
+        public string MoTa { get; set; }
+        [Display(Name = "Thời Gian Bắt Đầu")]
+        public DateTime ThoiGianBatDau { get; set; }
+        [Display(Name = "Thời Gian Kết Thúc")]
+        public DateTime ThoiGianKetThuc { get; set; }
+    }
+}
